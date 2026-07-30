f1bcaf86a4d0855686ba272a0dfbe2d317c43eac
Aishwarya H <aishwaryahaishwaryah8@gmail.com>
2026-07-31 00:15:04 +0530

    Update Highest_Salary.sql

--- Highest_Salary.sql
+++ Highest_Salary.sql
@@ -27,3 +27,15 @@ VALUES
 -- Solution
 SELECT MAX(salary) AS highest_salary
 FROM employee;
+
+/*
+
+Find the second highest salary from the employee table.
+*/
+
+SELECT MAX(salary) AS second_highest_salary
+FROM employee
+WHERE salary < (
+    SELECT MAX(salary)
+    FROM employee
+);
